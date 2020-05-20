77c30e285171eeb638d2d43c0e323334774db18d
Guilherme Amado <guiii@live.com>
2020-05-20 19:49:58 -0300

    Added length check & password check

--- script.js
+++ script.js
@@ -18,9 +18,14 @@ function showSuccess(input, message) {
     formControl.classList.add('success');
 }
 
-function isValidEmail(email) {
+function isValidEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid')
+    }
 }
 
 function checkRequiredFields(inputArray) {
@@ -33,9 +38,29 @@ function checkRequiredFields(inputArray) {
     });
 }
 
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${input.placeholder} must be at least ${min} characters`)
+    } else if (input.value.length > max) {
+        showError(input, `${input.placeholder} must be less than ${max} characters`)
+    } else {
+        showSuccess(input);
+    }
+}
+
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+    }
+}
+
 // Event listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     checkRequiredFields([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    isValidEmail(email);
+    checkPasswordsMatch(password, password2);
 });
